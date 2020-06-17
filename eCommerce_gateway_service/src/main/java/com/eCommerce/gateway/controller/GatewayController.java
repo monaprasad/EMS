0aaea1994247eaf88c8a8aad7c4707dbfa7e3f29
monika Prasad <ajit.kumar@atcs.com>
2020-06-18 02:58:31 +0530

    cloud config

--- eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
+++ eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
@@ -1,6 +1,7 @@
 package com.eCommerce.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,19 @@ public class GatewayController {
 
 	@Autowired
 	CloudConfig cloudConfig;
+	
+	@Value("${welcome.message}")
+	private String welcomeText;
+	 
 
 	@GetMapping("/api/")
 	public String getName() {
 		return cloudConfig.getName();
 	}
 
+	
+	@GetMapping("/welCome/")
+	public String welCome() {
+		return welcomeText;
+	}
 }
