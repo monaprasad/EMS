078ee746d54ab18154a11430f44d5bca13012516
monika Prasad <ajit.kumar@atcs.com>
2020-06-18 01:10:40 +0530

    external cloud file config

--- eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
+++ eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
@@ -1,17 +1,20 @@
 package com.eCommerce.gateway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eCommerce.gateway.config.CloudConfig;
 
 @RestController
 public class GatewayController {
 
-	
-	
+	@Autowired
+	CloudConfig cloudConfig;
 
 	@GetMapping("/api/")
 	public String getName() {
-		return "Gateway";
+		return cloudConfig.getName();
 	}
 
 }
