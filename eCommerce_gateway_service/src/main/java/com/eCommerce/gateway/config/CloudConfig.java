44c37e203525db950ea000be0a997e869eb501bc
monika Prasad <ajit.kumar@atcs.com>
2020-07-01 17:28:28 +0530

    spring security

--- eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/config/CloudConfig.java
+++ eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/config/CloudConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix="com.cloud.portName")
+//@ConfigurationProperties(prefix="com.cloud.portName")
 public class CloudConfig {
 	
 	private String name;
--- eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
+++ eCommerce_gateway_service/src/main/java/com/eCommerce/gateway/controller/GatewayController.java
@@ -2,12 +2,17 @@ package com.eCommerce.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eCommerce.gateway.config.CloudConfig;
 
-@RestController
+@Controller
 public class GatewayController {
 
 	@Autowired
@@ -27,4 +32,20 @@ public class GatewayController {
 	public String welCome() {
 		return welcomeText;
 	}
+	
+//	@RequestMapping(value= "/login" , method = RequestMethod.GET)
+//	@ResponseBody
+//	@GetMapping("/login")
+//	public String getloginpage() {
+//		ModelAndView modeView = new ModelAndView("loginTwo");
+//		return "login";
+//	}
+	
+//	@RequestMapping(value= "/greeting" , method = RequestMethod.GET)
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=true, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
 }
